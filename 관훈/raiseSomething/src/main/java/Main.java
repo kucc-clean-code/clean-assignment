5d6097b95f18131c0b210c06b0b9891f18b43b53
riverandeye <riverandeye@gmail.com>
2020-03-14 11:15:16 +0900

    move folder

--- 관훈/raiseSomething/src/main/java/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-  public static void main(String[] args) {
-    System.out.println("================= KUCC 키우기 게임을 시작합니다. ==================");
-    System.out.println("======= help 명령어를 통해 사용할 수 있는 커맨드를 확인하세요 =======");
-    Game.start();
-    System.out.println("플레이해주셔서 감사합니다.");
-  }
-}
